3d8e81cad6cf9346e9843e4ff0c88b3b523d9988
Sergio Peralta <seperalta88@gmail.com>
2023-08-06 22:55:31 -0300

    config modules

---
+++ bitacora.md
@@ -0,0 +1,11 @@
+# Instalaciones 
+
+```bash	
+ pn i @nuxtjs/supabase @nuxtjs/tailwindcss @pinia/nuxt @prisma/client @vite-pwa/nuxt nuxt-icon prisma uuid
+```
+
+por las dudas:
+
+```bash
+pn i pinia -f
+```
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  pages: true,
+  devtools: { enabled: true },
+  modules: [
+    "nuxt-icons",
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@nuxtjs/supabase",
+    "@vite-pwa/nuxt",
+  ],
+  runtimeConfig: {
+    public: {
+      bucketUrl: process.env.BUCKET_URL,
+    },
+  },
+});
