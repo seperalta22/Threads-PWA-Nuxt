1ec0ddd532dbc914798e3556a09c55c1f749820e
Sergio Peralta <seperalta88@gmail.com>
2023-08-07 07:09:46 -0300

    Update Homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <script setup>
 import MainLayout from "@/layouts/MainLayout.vue";
+import { useUserStore } from "@/stores/user";
+const userStore = useUserStore();
+// const user = useSupabaseUser();
+
+let posts = ref([]);
+let isPosts = ref(false);
+let isLoading = ref(false);
 </script>
 
 <template>
   <MainLayout>
-    <h1>Index</h1>
+    <div id="IndexPage" class="w-full overflow-auto">
+      <div class="mx-auto max-w-[500px] overflow-hidden">
+        <div id="Posts" class="px-4 max-w-[600px] mx-auto">
+          <div v-if="isPosts" v-for="post in posts" :key="post">
+            {{ post }}
+          </div>
+        </div>
+      </div>
+    </div>
   </MainLayout>
 </template>
